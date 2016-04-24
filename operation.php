c7daca83d5baffce4328baf2beeed7eb92a3890a
mknszc <ozcan.camoglu@bil.omu.edu.tr>
2016-04-24 18:17:52 +0300

    add

---
+++ operation.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ÖZ
+ * Date: 24.04.2016
+ * Time: 17:37
+ */
+class operation {
+    public $result = 0;
+
+    /*
+     * The default global variable
+     *
+     */
+
+    function addition($number) {
+        $this->result = $this->result + $number;
+        return $this;
+    }
+
+    function subtraction($number) {
+        $this->result = $this->result - $number;
+        return $this;
+    }
+
+    function division($number) {
+        $this->result = $this->result / $number;
+        return $this;
+    }
+
+    function multiplication($number) {
+        $this->result = $this->result * $number;
+        return $this;
+    }
+
+    function result() {
+        echo $this->result;
+    }
+}
+
+/*
+ * example object
+ */
+$example = new operation();
+$example->addition(10)->subtraction(5)->division(3)->multiplication(2)->result(); ?>
