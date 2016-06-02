6d9690f6101be96764244aa6eb6f144916e88816
mknszc <ozcan.camoglu@bil.omu.edu.tr>
2016-06-03 00:19:42 +0300

    autoload and check methods

---
+++ autoload.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ÖZ
+ * Date: 02.06.2016
+ * Time: 23:17
+ */
+
+
+function __autoload($name) {
+
+    try {
+        require($name . ".php");
+    }
+    catch(Exception $e) {
+        return $e->getMessage();
+        die("ERROR");
+
+    }
+}
+
+function classCheck($class) {
+    return class_exists("$class") ? "Yes" : "No";
+}
+
+
+/**
+ * @param $class
+ * @param $name
+ * @return string
+ */
+function methodCheck($class, $name) {
+
+    try {
+        $exp = new $class();
+        return method_exists($exp, "$name") ? "Yes": "No";
+    }
+    catch(Exception $e) {
+        return $e->getMessage();
+        die("ERROR");
+
+    }
+}
+
+
+
+
