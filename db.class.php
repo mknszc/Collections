cce76302dc9bddab12a00ae037d159d5b121e11f
Özcan ÇAMOĞLU <ozcan.camoglu@bil.omu.edu.tr>
2016-06-10 11:26:26 +0300

    Update db.class.php

--- db.class.php
+++ db.class.php
@@ -1,59 +1,59 @@
 <?php
 
 	class db {
-        var $connection;
+        	var $connection;
 		public $result;
 		private static $instance = null;
 		
         public function __construct($connectionString) {
-            $this->connection = pg_connect($connectionString);
+            	$this->connection = pg_connect($connectionString);
         }
 		
         function addOrUpdate($query) {
-            return pg_query($this->connection, $query);
+            	return pg_query($this->connection, $query);
         }
 	
         function getRow($query) {
-            $result = pg_query($this->connection, $query);
-            return pg_fetch_row($result);
-		    }	
+            	$result = pg_query($this->connection, $query);
+            	return pg_fetch_row($result);
+	}	
         
         function getRowAsArray($query) {
-            $result = pg_query($this->connection, $query);
-            return pg_fetch_array($result);
+            	$result = pg_query($this->connection, $query);
+            	return pg_fetch_array($result);
         }
 		
         function getRowAsObject($query) {
-            $result = pg_query($this->connection, $query);
-            return pg_fetch_object($result);
+        	 $result = pg_query($this->connection, $query);
+        	 return pg_fetch_object($result);
         }
 		
         function getAll($query) {
-            $result = pg_query($this->connection, $query);
-            return pg_fetch_all($result);
+        	 $result = pg_query($this->connection, $query);
+            	return pg_fetch_all($result);
         }
 		
 	function getRowAsAssoc($query) {
-            $result = pg_query($this->connection, $query);
-            return pg_fetch_assoc($result);
+            	$result = pg_query($this->connection, $query);
+        	 return pg_fetch_assoc($result);
         }
 		
         function delete($query) {
-            return pg_exec($this->connection, $query);
+            	return pg_exec($this->connection, $query);
         }
 		
         function closeConnection() {
-            pg_close($this->connection);
+            	pg_close($this->connection);
         }
 		
         function dbError($result) {
-            echo pg_result_error($result);
+            	echo pg_result_error($result);
         }
 		
 	public static function getInstance($connectionString) {
 		if (!self::$instance) {
 			self::$instance = new db($connectionString);
-		    }
-			return self::$instance;
 		}
+		return self::$instance;
+	}
     }
